717beaac04698bca152a3e9ebbf36e93e2a0b5ed
Isumi <isumisw@gmail.com>
2026-02-19 21:45:00 +0400

    chore: clean up initial commented code

--- app/db/users.py
+++ app/db/users.py
@@ -17,15 +17,6 @@ class UserResource:
     def __init__(self):
         self.collection = DB.get_collection(USER_COLLECTION)
 
-#    def get_students(self, name: str | None = None, seniority: str | None = None):
-#        query = {}
-#        if name is not None:
-#            query[NAME] = {"$regex": name}
-#        if seniority is not None:
-#            query[SENIORITY] = seniority
-#
-#        students = self.collection.find(query)
-#        return serialize_items(list(students))
 
     def create_user(self, username: str, email: str, password_hash: str, phone: str | None = None, role: str = "member"):
         user = {
@@ -38,17 +29,6 @@ class UserResource:
         result = self.collection.insert_one(user)
         return str(result.inserted_id)
 
-#    def update_student(self, lookupemail: str, name: str, email: str, seniority: str):
-#        student_record = self.get_student_by_email(lookupemail)
-#
-#        if student_record is None:
-#            return None
-#
-#        new_data = {NAME: name, EMAIL: email, SENIORITY: seniority}
-#        result = self.collection.update_one({EMAIL: lookupemail}, {"$set": new_data})
-#
-#        return result
-
     def get_user_by_username(self, username: str):
         user = self.collection.find_one({USERNAME: username})
         return serialize_item(user)
@@ -56,12 +36,3 @@ class UserResource:
     def get_user_by_email(self, email: str):
         user = self.collection.find_one({EMAIL: email})
         return serialize_item(user)
-
-#    def delete_all_students(self):
-#        self.collection.delete_many({})
-#
-#    def add_multiple_students(self, students: list[dict]):
-#        if not students:
-#            return
-#
-#        self.collection.insert_many(students)
